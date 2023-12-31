a8b67b65e0c2ad9b33f1fdc8e1bc8dba9090f74e
GH <gwanghyunjeon@gmail.com>
2023-12-31 22:28:27 +0900

    Abstract 추가 & 템플릿만 먼저 생성완료

---
+++ aa-test/on-chain/src/actions/uniswap/uniswapV2/UniswapV2ExactOut.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {ActionBase} from "../../ActionBase.sol";
+// Out이 ETH라면 로직 완료 후 Unwrap
+// https://docs.uniswap.org/contracts/v2/reference/smart-contracts/router-02#swaptokensforexacttokens
+
+contract UniswapV2ExactOut is ActionBase {
+    function executeAction(
+        bytes memory _params,
+        uint8[][] memory _paramMapping,
+        bytes32[] memory _returnValues
+    )
+        public
+        payable
+        override
+        returns (bytes32){}
+
+    function executeAction(
+        uint256 _queueId,
+        bytes[] calldata _params
+    )
+        public
+        payable
+        override
+        returns (bytes32){}
+
+    function executeActionDirect(bytes memory _params) public payable override{}
+
+    function actionType() public pure override returns (uint8){}
+}
