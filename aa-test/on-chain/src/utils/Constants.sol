ceeacaae5079952bcda7a2878c54926d846ec68a
GH <gwanghyunjeon@gmail.com>
2024-03-27 18:35:50 +0900

    done

--- aa-test/on-chain/src/utils/Constants.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-import { ConstantsSepolia } from "./constants/ConstantsSepolia.sol";
-
-contract Constants is ConstantsSepolia { }
--- aa-test/on-chain/src/utils/constants/ConstantsMainnet.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-contract ConstantsMainnet {
-    address internal constant UREGISTRY_ADDRESS = 0x0000000000000000000000000000000000000000;
-    address internal constant UEVENTLOGGER_ADDRESS = 0x0000000000000000000000000000000000000000;
-    address internal constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address internal constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-}
